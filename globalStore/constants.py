15c71a255a598e9003c05ed21a974f8745091c2a
damonxiong <xiongxiangquan@gmail.com>
2023-08-21 17:30:48 +0800

    remove unuseful constant

--- globalStore/constants.py
+++ globalStore/constants.py
@@ -5,23 +5,9 @@ All global constants
 import sys
 sys.path.append('../')
 
-import globalStore.privateConstants as privateConstants
-
 ### All global constants here
 
 # enter paths to the correct secret files
-
-TWITTER_SECRETS = {
-    'nanbhas': privateConstants.TWITTER_SECRET_FILE_NAB,
-    'medai': privateConstants.TWITTER_SECRET_FILE_MEDAI,
-    'test': privateConstants.TWITTER_SECRET_FILE_TEST,
-}
-
-NOTION_SECRETS = {
-    'nanbhas': privateConstants.NOTION_SECRET_FILE_NAB,
-    'medai': privateConstants.NOTION_SECRET_FILE_MEDAI,
-    'test': privateConstants.NOTION_SECRET_FILE_TEST,
-}
 
 SUPPORT_PLATFORM = {
     'reddit': 'Reddit 👽',
