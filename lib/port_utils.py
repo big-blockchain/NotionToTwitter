4a330503c9c6f3633e75d19f9aef6b9c36c8ef59
damonxiong <xiongxiangquan@gmail.com>
2023-08-24 22:18:46 +0800

    add long text to thread

--- lib/port_utils.py
+++ lib/port_utils.py
@@ -8,6 +8,7 @@ import re
 import sys
 import time
 import traceback
+import textwrap
 from io import BytesIO
 
 from PIL import Image
@@ -234,40 +235,120 @@ def post_row_to_twitter(row, api_v1, api_v2, notion):
                 media_ids = None
 
             # post tweet with a reference to uploaded image as a reply to the replyToID
-            try:
-                if reply_to_id:
-                    r = api_v2.create_tweet(
-                        text=tweet_text, in_reply_to_tweet_id=reply_to_id, media_ids=media_ids)
-                else:
-                    print("retweetURLretweetURLretweetURL", retweet_url)
-                    username, tweet_id = extract_twitter_info(retweet_url)
+            if reply_to_id:
+                max_length = 270
+                if len(tweet_text) > max_length:
+                    # 使用正则表达式将长文本分割为段落
+                    paragraphs = re.split(r'\n', tweet_text)
 
+                    # 分割后的片段列表
+                    fragments = []
+
+                    # 当前片段的文本
+                    current_fragment_text = ""
+                    for paragraph in paragraphs:
+                        if len(current_fragment_text + paragraph) <= max_length:
+                            current_fragment_text += (paragraph + '\n')
+                        else:
+                            if len(current_fragment_text) > 0:
+                                fragments.append(current_fragment_text)
+                                current_fragment_text = ""
+                            if len(paragraph) <= max_length:
+                                current_fragment_text = (paragraph + '\n')
+                            else:
+                                # 使用正则表达式将段落分割为句子
+                                sentences = re.split(r'(?<=[.!?\n])\s+', paragraph)
+                                for sentence in sentences:
+                                    # 如果当前片段的文本加上当前句子不超过最大长度，则将句子添加到当前片段的文本中
+                                    if len(current_fragment_text + sentence) <= max_length:
+                                        current_fragment_text += (sentence + '\n')
+                                    # 否则，将当前片段添加到片段列表中，并开始一个新的片段
+                                    else:
+                                        fragments.append(current_fragment_text)
+                                        current_fragment_text = (sentence + '\n')
+
+                                # 将最后一个片段添加到片段列表中
+                                fragments.append(current_fragment_text)
+
+                    parent_tweet = reply_to_id
+                    for tweet_text in fragments:
+                        if parent_tweet is None and len(media_ids) > 0:
+                            print(len(tweet_text))
+                            r = api_v2.create_tweet(text=tweet_text, media_ids=media_ids)
+                            parent_tweet = r.data['id']
+                        else:
+                            print(len(tweet_text))
+                            r = api_v2.create_tweet(text=tweet_text, in_reply_to_tweet_id=parent_tweet)
+                            parent_tweet = r.data['id']
+                else:
                     r = api_v2.create_tweet(
                         text=tweet_text, in_reply_to_tweet_id=tweet_id, media_ids=media_ids)
-                # update error text
-                if r.errors:
-                    # 将错误信息记录到 Notion 页面的 "Error Message" 属性中
-                    error_messages = [error['message'] for error in r.errors]
-                    print('UPDATE STATUS FAILURE: '.join(error_messages))
-                    tweeted = False
-                    break
-                else:
-                    print('UPDATE TWITTER STATUS SUCCESS'.join(row.title))
+            else:
+                print("retweetURLretweetURLretweetURL", retweet_url)
+                username, tweet_id = extract_twitter_info(retweet_url)
+                max_length = 270
+                if len(tweet_text) > max_length:
+                    # 使用正则表达式将长文本分割为段落
+                    paragraphs = re.split(r'\n', tweet_text)
 
-                # update reply to ID
-                reply_to_id = r.data["id"]  # 不存在的话抛出错误 Keyerror
-                # replyToID = data.get("id")
-                # thread tweet ID
-                if first_tweet:
-                    first_tweet = False
-            except HTTPException as e:
+                    # 分割后的片段列表
+                    fragments = []
+
+                    # 当前片段的文本
+                    current_fragment_text = ""
+                    for paragraph in paragraphs:
+                        if len(current_fragment_text + paragraph) <= max_length:
+                            current_fragment_text += (paragraph + '\n')
+                        else:
+                            if len(current_fragment_text) > 0:
+                                fragments.append(current_fragment_text)
+                                current_fragment_text = ""
+                            if len(paragraph) <= max_length:
+                                current_fragment_text = (paragraph + '\n')
+                            else:
+                                # 使用正则表达式将段落分割为句子
+                                sentences = re.split(r'(?<=[.!?\n])\s+', paragraph)
+                                for sentence in sentences:
+                                    # 如果当前片段的文本加上当前句子不超过最大长度，则将句子添加到当前片段的文本中
+                                    if len(current_fragment_text + sentence) <= max_length:
+                                        current_fragment_text += (sentence + '\n')
+                                    # 否则，将当前片段添加到片段列表中，并开始一个新的片段
+                                    else:
+                                        fragments.append(current_fragment_text)
+                                        current_fragment_text = (sentence + '\n')
+
+                                # 将最后一个片段添加到片段列表中
+                                fragments.append(current_fragment_text)
+
+                    parent_tweet = tweet_id
+                    for tweet_text in fragments:
+                        if parent_tweet is None and len(media_ids) > 0:
+                            print(len(tweet_text))
+                            r = api_v2.create_tweet(text=tweet_text, media_ids=media_ids)
+                            parent_tweet = r.data['id']
+                        else:
+                            print(len(tweet_text))
+                            r = api_v2.create_tweet(text=tweet_text, in_reply_to_tweet_id=parent_tweet)
+                            parent_tweet = r.data['id']
+                else:
+                    r = api_v2.create_tweet(
+                        text=tweet_text, in_reply_to_tweet_id=tweet_id, media_ids=media_ids)
+            # update error text
+            if r.errors:
+                # 将错误信息记录到 Notion 页面的 "Error Message" 属性中
+                error_messages = [error['message'] for error in r.errors]
+                print('UPDATE STATUS FAILURE: '.join(error_messages))
                 tweeted = False
-                # 打印错误的堆栈信息
-                traceback.print_exc()
-                # 获取错误的类型和值
-                err_type, err_value = sys.exc_info()[:2]
-                print('UPDATE STATUS FAILURE:', str(e.response.json()), err_type, err_value)
-                pass
+                break
+            else:
+                print('UPDATE TWITTER STATUS SUCCESS'.join(row.title))
+
+            # update reply to ID
+            reply_to_id = r.data["id"]  # 不存在的话抛出错误 Keyerror
+            # replyToID = data.get("id")
+            # thread tweet ID
+            if first_tweet:
+                first_tweet = False
 
         # update Notion
         if tweeted:
